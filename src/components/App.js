0d524fe804881ed13629e17dbd188a3a02e9a250
dr1pryaha <dr.pryah@gmail.com>
2019-04-10 15:34:34 +0300

    Change .gitignore, add folder dist

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React,{Component} from 'react';
 import '../styles/App.css';
 import EntryField from './EntryField.js';
+import ButtonList from './ButtonList.js';
  
 class App extends Component{
   render(){
     return (
     	<div>
     		<EntryField />
+    		<ButtonList />
     	</div>
     );
   }
