563bdd817f234aa7dccc59e123f0538894484c38
Anton Pryakhin <dr.pryah@yandex.ru>
2019-04-11 22:34:24 +0300

    Change App.js, EntryField.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,37 @@ import React,{Component} from 'react';
 import '../styles/App.css';
 import EntryField from './EntryField.js';
 import ButtonList from './ButtonList.js';
- 
-class App extends Component{
-  render(){
-    return (
-    	<div>
-    		<EntryField />
-    		<ButtonList />
-    	</div>
-    );
-  }
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.isEnterPressed = this.isEnterPressed.bind(this);
+		this.state = {text: ''};
+	}
+
+	isEnterPressed(e) {
+		if (e.key === 'Enter') {
+			this.setState({text: ''});
+		}
+	}
+
+	handleChange(e) {
+		this.setState({text: e.target.value});
+	}
+
+	render(){
+		return (
+			<div>
+				<EntryField 
+					value={this.state.text}
+					onChange={this.handleChange} 
+					onKeyPress={this.isEnterPressed}
+				/>
+				<ButtonList />
+			</div>
+	);
+	}
 }
- 
+
 export default App;
