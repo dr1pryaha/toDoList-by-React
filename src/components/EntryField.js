48f96a6c7f1e85bad3ca709ff89880f8c8fd2870
dr1pryaha <dr.pryah@gmail.com>
2019-04-11 13:26:54 +0300

    Add handleChange, isEnterPressed in EntryField

--- src/components/EntryField.js
+++ src/components/EntryField.js
@@ -1,12 +1,36 @@
 import React,{Component} from 'react';
 
 class EntryField extends Component{
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.isEnterPressed = this.isEnterPressed.bind(this);
+		this.state = {text: ''};
+		}
+
+	isEnterPressed(e) {
+		if (e.key === 'Enter') {
+      		this.setState({text: ''});
+    	}
+  	}
+
+	handleChange(e) {
+		this.setState({text: e.target.value});
+  }
+
 	render(){
+		const text = this.state.text;
 		return (
 			<div>
     			<h1>toDos</h1>
     			<div className = 'head-field'>
-        			<input type='search' className = 'entry-field' autoFocus />
+        			<input 
+        			type='search' 
+        			value={text} 
+        			onChange={this.handleChange} 
+        			onKeyPress={this.isEnterPressed} 
+        			className = 'entry-field' 
+        			autoFocus />
     			</div>
     		</div>
 		);
