cfa803223773e7349fe449ffd1428bf915d0469b
dr1pryaha <dr.pryah@gmail.com>
2019-04-12 14:54:24 +0300

    Bug isEnterPressed

--- src/components/EntryField.js
+++ src/components/EntryField.js
@@ -2,13 +2,21 @@ import React,{Component} from 'react';
 
 class EntryField extends Component{
 	render(){
+		console.log(this.props);
 		return (
 			<div>
-    			<h1>toDos</h1>
-    			<div className = 'head-field'>
-        			<input type='search' className = 'entry-field' autoFocus />
-    			</div>
-    		</div>
+				<h1>toDos</h1>
+				<div className = 'head-field'>
+					<input 
+						type = 'search' 
+						className = 'entry-field' 
+						autoFocus
+						value = {this.props.text}
+						onChange={this.props.onChange} 
+						onKeyDown={this.props.onKeyDown}
+					/>
+				</div>
+			</div>
 		);
 	}
 }
