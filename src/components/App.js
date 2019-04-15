d6882ab1e823bb42163c3e7ad3a9fcf26ed57884
dr1pryaha <dr.pryah@gmail.com>
2019-04-15 13:31:39 +0300

    Add ToDoList.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import '../styles/App.css';
 import EntryField from './EntryField.js';
 import ButtonList from './ButtonList.js';
+import ToDoList from './ToDoList.js';
  
 class App extends Component {
 	constructor(props) {
@@ -9,6 +10,8 @@ class App extends Component {
 		this.handleChange = this.handleChange.bind(this);
 		this.isEnterPressed = this.isEnterPressed.bind(this);
 		this.state = {text: ''};
+		const toDoItem = [];
+		this.createToDoItem = this.createToDoItem.bind(this);
 	}
 
 	isEnterPressed(e) {
@@ -21,6 +24,12 @@ class App extends Component {
 		this.setState({text: e.target.value});
 	}
 
+	createToDoItem(e) {
+		if (isEnterPressed()) {
+			toDoItem.push(<ToDoList/>)
+		}
+	}
+
 	render(){
 		return (
 			<div>
@@ -28,6 +37,9 @@ class App extends Component {
 					value={this.state.text}
 					onChange={this.handleChange} 
 					onKeyDown={this.isEnterPressed}
+				/>
+				<ToDoList
+					className = 'to-do-item'
 				/>
 				<ButtonList />
 			</div>
---
+++ src/components/ToDoList.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class ToDoList extends Component{
+	render(){
+		return (
+			<div>
+				<section className = 'to-do-list'>
+					
+				</section>
+			</div>
+		);
+	}
+}
+
+export default ToDoList;
